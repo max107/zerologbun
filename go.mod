68d047a8b2027759eef2aedc5f9dfab019bd4456
Falaleev Maksim <maxfalaleev1@gmail.com>
2021-11-09 09:11:10 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/max107/zerologbun
+
+go 1.17
+
+require (
+	github.com/rs/zerolog v1.26.0
+	github.com/uptrace/bun v1.0.16
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+)
---
+++ logger.go
@@ -0,0 +1,67 @@
+package dblogger
+
+import (
+	"context"
+	"github.com/rs/zerolog"
+	"github.com/uptrace/bun"
+	"strings"
+	"time"
+)
+
+type QueryHook struct {
+	logger zerolog.Logger
+}
+
+func NewQueryHook(logger zerolog.Logger) *QueryHook {
+	return &QueryHook{logger}
+}
+
+func (h *QueryHook) BeforeQuery(ctx context.Context, event *bun.QueryEvent) context.Context {
+	return ctx
+}
+
+func (h *QueryHook) AfterQuery(ctx context.Context, event *bun.QueryEvent) {
+	dur := time.Now().Sub(event.StartTime)
+
+	l := h.logger.
+		With().
+		Str("type", queryOperation(event.Query)).
+		Str("query", event.Query).
+		Str("operation", eventOperation(event)).
+		Str("duration", dur.String()).
+		Logger()
+
+	if event.Err == nil {
+		l.Info().Msg("query")
+	} else {
+		l.Error().Err(event.Err).Msg("query")
+	}
+}
+
+func eventOperation(event *bun.QueryEvent) string {
+	switch event.IQuery.(type) {
+	case *bun.SelectQuery:
+		return "SELECT"
+	case *bun.InsertQuery:
+		return "INSERT"
+	case *bun.UpdateQuery:
+		return "UPDATE"
+	case *bun.DeleteQuery:
+		return "DELETE"
+	case *bun.CreateTableQuery:
+		return "CREATE TABLE"
+	case *bun.DropTableQuery:
+		return "DROP TABLE"
+	}
+	return queryOperation(event.Query)
+}
+
+func queryOperation(name string) string {
+	if idx := strings.Index(name, " "); idx > 0 {
+		name = name[:idx]
+	}
+	if len(name) > 16 {
+		name = name[:16]
+	}
+	return name
+}
